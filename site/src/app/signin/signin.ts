311234d64d91d148f7b0143bf967aa1d8f65de5e
Kaan <kaan@Kaans-Mac-mini.local>
2025-06-19 18:02:22 +0300

    3 Sites has been introduced along with password & overrall form validations beforehand, awaiting nestjs integration

--- site/src/app/signin/signin.ts
+++ site/src/app/signin/signin.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './signin.html',
   styleUrl: './signin.scss'
 })
 export class Signin {
 
+  email: string = '';
+  password: string = '';
+
+  isFormValid(): boolean {
+    return this.email.length > 0 && 
+    this.password.length > 5 &&
+    this.isValidEmail();
+  }
+
+  onSubmit(): void {
+    if (this.isFormValid()){}
+  }
+  isValidEmail(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(this.email);
+  }
 }
--- site/src/app/signup/signup.ts
+++ site/src/app/signup/signup.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
 export class Signup {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  passwordsMatch(): boolean {
+    return this.password === this.confirmPassword;
+  }
+
+  isValidEmail(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(this.email);
+  }
+
+  isFormValid(): boolean {
+    return this.email.length > 0 && 
+           this.password.length > 5 && 
+           this.confirmPassword.length > 5 && 
+           this.passwordsMatch() &&
+           this.isValidEmail();
+  }
+
+  onSubmit(): void {
+    if (this.isFormValid()){
+      
+    }
+  }
 }
